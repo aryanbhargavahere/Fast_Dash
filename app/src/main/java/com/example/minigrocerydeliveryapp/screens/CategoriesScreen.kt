b2766b669a8027bc93638ea677272a2f74c1fd5d
aryanbhargavahere <aryanbhargava783@gmail.com>
2026-05-11 22:51:37 +0530

    made updates

--- app/src/main/java/com/example/minigrocerydeliveryapp/screens/CategoriesScreen.kt
+++ app/src/main/java/com/example/minigrocerydeliveryapp/screens/CategoriesScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
